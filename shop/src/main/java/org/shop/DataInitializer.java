76e0ce3e7cf574634e91080ad227fe88e9382801
Dzmitry_Naskou <Dzmitry_Naskou@epam.com>
2012-10-06 17:42:10 -0430

    data initializer

--- shop/src/main/java/org/shop/DataInitializer.java
+++ shop/src/main/java/org/shop/DataInitializer.java
@@ -1,25 +1,35 @@
 package org.shop;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  * The main Data Initializer util class.
  */
+@Named
 public class DataInitializer {
 
     /** The seller initializer. */
+    @Inject
     private SellerInitializer sellerInitializer;
     
     /** The product initializer. */
+    @Inject
     private ProductInitializer productInitializer;
     
     /** The proposal initializer. */
+    @Inject
     private ProposalInitializer proposalInitializer;
     
     /** The user initializer. */
+    @Inject
     private UserInitializer userInitializer;
 
     /**
      * Inits the data.
      */
+    @PostConstruct
     public void initData() {
         sellerInitializer.initSellers();
         userInitializer.initUsers();
